d7583c003b72a169b46b565f138cca229120fc36
felipeurien <urienfelipe@gmail.com>
2022-11-14 19:05:52 -0300

    primer paso front

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,3 +1,40 @@
-function rootReducer() {}
+const initialState = {
+  videogames: [],
+  allVideogames: [],
+  createdInDb: [],
+  genres: [],
+  platforms: [],
+  detail: [],
+  currentPage: 1,
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_VIDEOGAMES":
+      return {
+        ...state,
+        videogames: action.payload,
+        allVideogames: action.payload,
+      };
+
+    case "GET_NAME_VIDEOGAMES":
+      return {
+        ...state,
+        videogames: action.payload,
+      };
+
+    case "GET_DETAIL":
+      return {
+        ...state,
+        detail: action.payload,
+      };
+
+    case "CLEAR_DETAIL":
+      return {
+        ...state,
+        detail: [],
+      };
+  }
+}
 
 export default rootReducer;
